881e317dae85dfb644ab0301b8f8daba62693441
manish-4007 <manish.rai709130@gmail.com>
2023-09-02 03:23:28 +0530

    requirements and dependencies

--- Home.py
+++ Home.py
@@ -34,12 +34,12 @@ def intall_ffmpeg():
         # lib_path = "/usr/local/bin/ffmpeg"
         # lib_path = "/opt/ffmpeg/bin/ffmpeg"
 
-        # import pydub,ffmpeg 
+        import pydub,ffmpeg 
         # ffmpeg_binary = f"{lib_path}"
         # ffprobe_binary = f"{lib_path}/ffprobe.exe"
         # pydub.AudioSegment.ffmpeg = ffmpeg_binary
         # ffmpeg.input.ffmpeg = "/usr/bin/ffmpeg.exe"
-        # pydub.AudioSegment.converter = '/usr/bin/ffprobe'
+        pydub.AudioSegment.converter = '/usr/bin/ffprobe.exe'
         # # ffmpeg.input.ffprobe = '/usr/bin/ffprobe'
 
         print("ffmpeg and ffprobe setup done completely.")
