fb4a3dbce6e24daa0a1a086c512a98c37d9f2679
manish-4007 <manish.rai709130@gmail.com>
2023-09-02 16:56:40 +0530

    requirements and dependencies

--- img_magic.sh
+++ img_magic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+sudo chmod +w /etc/ImageMagick-6/policy.xml
 cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g' > /etc/ImageMagick-6/policy.xml
--- pages/Subtitle_Merger.py
+++ pages/Subtitle_Merger.py
@@ -321,10 +321,10 @@ def install_img_magic_commands_linux():
 
     # Use subprocess to execute the shell script
       try:
-          subprocess.run(['chmod', '+x', shell_script_path], check=True)
+          subprocess.run(['chmod', '+x', 'img_magic.sh'], check=True)
           st.write(f"File {shell_script_path} is now executable.")
 
-          st.write(subprocess.run(["./img_magic.sh"]))
+          st.write(subprocess.run(["sudo","./img_magic.sh"]))
 
           # # Check the return code for success or failure
           # if subprocess.returncode == 0:
