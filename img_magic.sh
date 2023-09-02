d2c518819cfe36e5c358ab819217bc3a67f43e98
manish-4007 <manish.rai709130@gmail.com>
2023-09-02 18:03:54 +0530

    requirements and dependencies

--- img_magic.sh
+++ img_magic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo chmod +w /etc/ImageMagick-6/policy.xml
-echo "Fetching Done"
 sudo chmod -R 777 /usr/
 sudo chmod -R 777 /root/
+echo "Fetching Done"
 # sudo -S cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g' > /etc/ImageMagick-6/policy.xml
 #!/bin/sh
 
