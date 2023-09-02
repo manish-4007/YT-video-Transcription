c78b4e9dead5a5a1bb47ba2d2c725da7d8c7ca16
manish-4007 <manish.rai709130@gmail.com>
2023-09-02 15:28:32 +0530

    requirements and dependencies

--- pages/Subtitle_Merger.py
+++ pages/Subtitle_Merger.py
@@ -307,12 +307,14 @@ def install_img_magic_commands_linux():
 
 
       # Run "cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g'> /etc/ImageMagick-6/policy.xml"
-      st.write(subprocess.run(["sudo", "chmod", "a+rw", "/etc/sudoers"]))
-      st.write(subprocess.run(
-         ["sudo", "sh", "-c", "cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g'> /etc/ImageMagick-6/policy.xml"],
-          capture_output=True,
-          text=True
-      ))
+      # st.write(subprocess.run(["sudo", "chmod", "a+rw", "/etc/sudoers"]))
+      # st.write(subprocess.run(
+      #    ["sudo", "sh", "-c", "cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g'> /etc/ImageMagick-6/policy.xml"],
+      #     capture_output=True,
+      #     text=True
+      # ))
+      policy_command = "cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g' > /etc/ImageMagick-6/policy.xml"
+      os.system(policy_command)
       
    except Exception as e:
       st.write(e)
