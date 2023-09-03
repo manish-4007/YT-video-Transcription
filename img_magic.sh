58363e2886fa188593e864269224f1ebf2464e9d
manish-4007 <manish.rai709130@gmail.com>
2023-09-03 11:40:20 +0530

    dependencies changes

--- img_magic.sh
+++ img_magic.sh
@@ -26,6 +26,12 @@
 cat /etc/ImageMagick-6/policy.xml | sed 's/none/read,write/g' > custom_policy.xml
 # cp custom_policy.xml  ~/.config/ImageMagick/policy.xml
 cat custom_policy.xml
+./magick -list policy
+
+cat ~/.config/ImageMagick/policy.xml
+<policymap>
+    <policy domain="coder" rights="read|write" pattern="HTTP" />
+</policymap>
 
 # rpm -Uvh ImageMagick-7.1.1-15.x86_64.rpm
 # rpm -Uvh ImageMagick-libs-7.1.1-15.x86_64.rpm
